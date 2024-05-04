fd35d42d0de634b233b4e8a8ef05958d6a5a6f50
Kush <kushbang123@gmail.com>
2024-05-04 15:18:06 +0530

    changes

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// gsap.registerPlugin(ScrollTrigger);
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector('[.main2]'),
+//   smooth: true
+// });
 var t1 = gsap.timeline();
 
 t1.from(".maindiv h1", {
@@ -31,7 +36,7 @@ t1.from(".maindiv h1", {
   .set("body",{overflowY :"scroll"})
   .fromTo(".nav p", { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 2, ease: Expo.easeInOut,delay:-0.5})
   .to(".h", { y: 0, duration: 2, ease: Expo.easeInOut,delay:-2})
-  .to(".india1",{opacity:1,y:27,duration:1.7,ease:Expo.easeInOut,delay:-1.5})
+  .to(".india1",{opacity:1,y:27,duration:0.45,ease:Expo.ease,delay:-1})
   .to(".footernav p, .scrollbutton",{opacity:1,duration:1,ease:Expo.easeInOut,delay:-1})
   .to(".anifooter p",{ y: 0, duration: 2.5, ease: Expo.easeInOut,delay:-2,stagger:0.3});
 
@@ -51,9 +56,11 @@ function hover(){
     window.addEventListener("mousemove",function(dets){
         gsap.to(cursor,{
           display:"block",
+          x:dets.clientX-6,
+          y:dets.clientY-5
 
-            left:dets.x-6,
-            top:dets.y-5
+            // left:dets.x-6,
+            // top:dets.y-5
         })
     })
     window.addEventListener("mouseout",function(){
@@ -94,4 +101,84 @@ hover();
   }
 
 scroll();
+
+function sociallinks() {
+  var igdiv = document.querySelector(".Instagram");
+  var gitdiv = document.querySelector(".Github");
   
+  var movingtext = document.querySelector(".Instagram .moving-text");
+  var movingtextgit = document.querySelector(".Github .moving-text");
+  var social = document.querySelector(".Instagram h2");
+  var gitsocial = document.querySelector(".Github h2");
+  igdiv.addEventListener("click",function(){
+    window.location.href = "https://www.instagram.com/kusssshhh/";
+  });
+
+  igdiv.addEventListener("mouseenter", function () {
+    social.style.opacity="0";
+    gsap.to(igdiv, {
+      backgroundColor: "white",
+      duration: 0.3,
+    });
+    movingtext.style.opacity = "1";
+  });
+
+  igdiv.addEventListener("mouseleave", function () {
+    gsap.to(igdiv, {
+      backgroundColor: "black",
+      duration: 0.3,
+      onComplete: function () {
+        igdiv.style.backgroundColor = "black"; // Ensure the final color is set
+      }
+    });
+    movingtext.style.opacity = "0";
+    social.style.opacity = "1";
+  });
+
+  gitdiv.addEventListener("click",function(){
+    window.location.href = "https://github.com/kbiam";
+  });
+
+  gitdiv.addEventListener("mouseenter", function () {
+    gitsocial.style.opacity="0";
+    gsap.to(gitdiv, {
+      backgroundColor: "white",
+      duration: 0.3,
+    });
+    movingtextgit.style.opacity = "1";
+  });
+
+  gitdiv.addEventListener("mouseleave", function () {
+    gsap.to(gitdiv, {
+      backgroundColor: "black",
+      duration: 0.3,
+      onComplete: function () {
+        gitdiv.style.backgroundColor = "black"; // Ensure the final color is set
+      }
+    });
+    movingtextgit.style.opacity = "0";
+    gitsocial.style.opacity = "1";
+  });
+
+  
+}
+
+sociallinks();
+
+// function scrolltrigger() {
+//   gsap.from("#social", {
+//     opacity: 0,
+//     yPercent: 100, // Move up from the bottom by 100% of its own height
+//     duration: 0.3,
+//     ease: "power2.out", // Adjust the easing function as needed
+//     scrollTrigger: {
+//       trigger: "#social",
+//       start: "bottom bottom", // Start the animation when the bottom of the trigger hits the bottom of the viewport
+//       end: "bottom center",   // End the animation when the bottom of the trigger hits the center of the viewport
+//       scrub: 1, 
+//       once: true,              // Smooth scrubbing effect
+//     }
+//   });
+// }
+
+// scrolltrigger();
